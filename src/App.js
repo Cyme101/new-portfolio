f63e40e0dc675ec56ec159d62bb414877f152c9a
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-09-29 16:22:15 -0400

    Added the contact, projects, resume, about me and my skills links, fixed the social links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./globalStyles";
 import { LightTheme } from "./components/Themes";
+
 import Main from "./components/Main";
 import AboutMePage from "./components/AboutMePage";
-import ProjectsPage from "./components/ProjectsPage";
+import ResumePage from "./components/MySkillsPage";
 import MySkillsPage from "./components/MySkillsPage";
+import ProjectsPage from "./components/ProjectsPage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={LightTheme}>
-        <Routes>
+        <Routes location={location} key={location.pathname}>
           <Route path="/" element={<Main />} />
           <Route path="/about" element={<AboutMePage />} />
+          <Route path="/resume" element={<ResumePage />} />
           <Route path="/skills" element={<MySkillsPage />} />
           <Route path="/projects" element={<ProjectsPage />} />
         </Routes>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 import LogoComponent from "../subComponents/LogoComponent";
 import PowerButton from "../subComponents/PowerButton";
 import SocialIcons from "../subComponents/SocialIcons";
@@ -25,14 +26,157 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
+const Contact = styled.a`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  top: 2rem;
+  z-index: 1;
+`;
+
+const RESUME = styled.a`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const PROJECTS = styled.a`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  left: calc(1rem + 2vw);
+  position: absolute;
+  text-decoration: none;
+  top: 50%;
+  transform: translate(-50%, -50%) rotate(-90deg);
+  z-index: 1;
+`;
+
+const BottomBar = styled.div`
+  bottom: 1rem;
+  display: flex;
+  justify-content: space-evenly;
+  left: 0;
+  position: absolute;
+  right: 0;
+  width: 100%;
+`;
+
+const ABOUT = styled(NavLink)`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  text-decoration: none;
+  z-index: 1;
+`;
+const SKILLS = styled(NavLink)`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+const Center = styled.button`
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  left: ${(props) => (props.click ? "92%" : "50%")};
+  outline: none;
+  position: absolute;
+  top: ${(props) => (props.click ? "85%" : "50%")};
+  transition: all 1s ease;
+  & > :last-child {
+    display: ${(props) => (props.click ? "none" : "inline-block")};
+    padding-top: 1rem;
+  }
+  transform: translate(-50%, -50%);
+`;
+
+const DarkDiv = styled.div`
+  background-color: #090909;
+  bottom: 0;
+  height: ${(props) => (props.click ? "100%" : "0%")};
+  position: absolute;
+  right: 50%;
+  top: 0;
+  transition: height 0.5s ease, width 1s ease 0.5s;
+  width: ${(props) => (props.click ? "50%" : "0%")};
+  z-index: 1;
+`;
+
 const Main = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
     <div>
       <MainContainer>
+        <DarkDiv click={click} />
         <Container>
           <PowerButton />
-          <LogoComponent />
-          <SocialIcons />
+          <LogoComponent theme={click ? "dark" : "light"} />
+          <SocialIcons theme={click ? "dark" : "light"} />
+          <Center click={click}>
+            <LogoComponent
+              onClick={() => handleClick()}
+              width={click ? 120 : 200}
+              height={click ? 120 : 200}
+              fill="currentColor"
+            />
+            <h3>
+              <span>Click Here!</span>
+            </h3>
+          </Center>
+          <Contact
+            style={{ color: "inherit" }}
+            target="_blank"
+            rel="noreferrer"
+            href="mailto:isabelle.vall@gmail.com"
+          >
+            <h3>Say Bonjour-Hi...</h3>
+          </Contact>
+          <RESUME
+            style={{ color: "inherit" }}
+            target="_blank"
+            rel="noreferrer"
+            href=""
+          >
+            <h3>RESUME.</h3>
+          </RESUME>
+          <PROJECTS
+            style={{ color: "inherit" }}
+            target="_blank"
+            rel="noreferrer"
+            to="/projects"
+            click={+click}
+          >
+            <h3>PROJECTS.</h3>
+          </PROJECTS>
+          <BottomBar>
+            <ABOUT
+              style={{ color: "inherit" }}
+              target="_blank"
+              rel="noreferrer"
+              to="/about"
+              click={+click}
+            >
+              <h3>ABOUT ME.</h3>
+            </ABOUT>
+            <SKILLS
+              style={{ color: "inherit" }}
+              target="_blank"
+              rel="noreferrer"
+              to="/skills"
+              click={+click}
+            >
+              <h3>MY SKILLS.</h3>
+            </SKILLS>
+          </BottomBar>
         </Container>
       </MainContainer>
     </div>
