7908fb41ac247fdfa9dedd32dff9060a84199112
“Isabelle” <isabelle.vall@gmail.com>
2022-11-08 14:47:43 -0500

    Added home button svg.

---
+++ src/subComponents/HomeButton.js
@@ -0,0 +1,52 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+import { HomeBtn } from "../components/AllSvgs";
+import { mediaQueries } from "../components/Themes";
+
+const Home = styled.button`
+  align-items: center;
+  background-color: #fffdfa;
+  border: 2px solid #090909;
+  border-radius: 50%;
+  cursor: pointer;
+  display: flex;
+  height: 2.5rem;
+  justify-content: center;
+  left: 50%;
+  padding: 0.4rem;
+  position: fixed;
+  top: 2rem;
+  transform: translate(-50%, 0);
+  width: 2.5rem;
+  z-index: 4;
+
+  &:hover {
+    background-color: rgba(0, 255, 245, 0.4);
+  }
+
+  & > *:first-child {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  ${mediaQueries(40)`
+    width: 2rem;
+    height: 2rem;
+      svg{
+        width: 20px;
+        height: 20px;
+      }
+  `};
+`;
+
+const HomeButton = () => {
+  return (
+    <Home>
+      <NavLink to="/">
+        <HomeBtn height={30} width={30} fill="currentColor" />
+      </NavLink>
+    </Home>
+  );
+};
+
+export default HomeButton;
